f03e9f1a1755a0620719c7efb81cea0114bebcb4
Alex Pate <proto.merkabah@gmail.com>
2015-01-16 12:08:56 -0500

    add css, make sure min, sec always two digits

--- clock.js
+++ clock.js
@@ -3,9 +3,18 @@ var display = document.getElementById("display");
 
 var on_tick = function() {
   var time = new Date();
-  var hour = time.getHours();
-  var min  = time.getMinutes();
-  var sec  = time.getSeconds();
+  var hour = time.getHours().toString();
+  var min  = time.getMinutes().toString();
+  var sec  = time.getSeconds().toString();
+
+  if (min.length == 1) {
+    min = "0" + min;
+  }
+
+  if (sec.length == 1) {
+    sec = "0" + sec;
+  }
+
   display.innerText = "" + hour + ":" + min + ":" + sec; 
 };
 
