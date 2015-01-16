57025d2f70a5d859b4886a85713fb7274a1393a6
Alex Pate <proto.merkabah@gmail.com>
2015-01-16 12:28:13 -0500

    Added double digits to hour

--- clock.js
+++ clock.js
@@ -7,6 +7,10 @@ var on_tick = function() {
   var min  = time.getMinutes().toString();
   var sec  = time.getSeconds().toString();
 
+  if (hour.length == 1) {
+    hour = "0" + hour;
+  }
+
   if (min.length == 1) {
     min = "0" + min;
   }
@@ -15,7 +19,7 @@ var on_tick = function() {
     sec = "0" + sec;
   }
 
-  display.innerText = "" + hour + ":" + min + ":" + sec; 
+  display.innerText = hour + ":" + min + ":" + sec; 
 };
 
 window.setInterval(on_tick, 1000);
